8fa67f3dc032e1da24585e615cfea43e139e830f
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-12-27 11:36:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-//your JS code here. If required.
+function getArrays() {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve([1,2,3,4]);
+		}, 3000);
+	});
+}
+function filterOutOddNumber(arr) {
+	return arr.filter((num) => num % 2 === 0);
+}
+function multiplyEvenNumber(arr) {
+	return arr.map((num) => num % 2 === 0 ? num*2 : num);
+}
+function updateText(text) {
+	const output = document.getElementById("output");
+	output.innerText = text;
+}
+
+getArrays()
+ .then((numbers) => {
+	 return new Promise((resolve) => {
+		setTimeout(() => {
+			const filterNumber = filterOutOddNumber(numbers);
+			updateText(`filter out odd number: ${filterNumber}`);
+			resolve(filterNumber);
+		}, 1000); 
+	 });
+ })
+ .then((filterNumber) => {
+	 return new Promise((resolve) => {
+		setTimeout(() => {
+			const multiplyNumber = multiplyEvenNumber(filterNumber);
+			updateText(`multiply even number: ${multiplyNumber}`);
+			resolve(multiplyNumber);
+		}, 2000); 
+	 });
+ })
+.catch((error) => {
+	console.error("Error: ", error);
+});
